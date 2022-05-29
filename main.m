426ad7b3ae0714e9b6e8ac22908a6c87adc1b7af
Stefan Bucher <web@stefan-bucher.ch>
2022-05-28 20:41:05 -0400

    allow importing data rather than running estimation

--- main.m
+++ main.m
@@ -8,65 +8,73 @@ close all;
 nImages = 25; %50;
 feature = 'orientation'; % Compare across 'orientation' or 'scale' 
 homogeneousSigma = 1; % 1: Pareto model fitted with restriction sigma1=sigma2.
+runEstimation = 1; % 0: does not run estimation, instead importing existing csv
 
 %% MLE of biavariate Pareto- & t-distributions to the filter responses to images from the van Hateren dataset
 
-parameterEstimates_Pareto = zeros(nImages,7);
-parameterEstimates_mvt = zeros(nImages,4);
+if ~ runEstimation
+    parameterEstimates_Pareto = csvread('parameterEstimates_Pareto.csv',1,0)
+    parameterEstimates_mvt = csvread('parameterEstimates_mvt.csv',1,0)
+else
+    parameterEstimates_Pareto = zeros(nImages,7);
+    parameterEstimates_mvt = zeros(nImages,4);
 
-for i=1:nImages
-    image = load('./VanHateren/im'+string(i)+'.mat').oim;
-    [c,b,hist_joint, hist_cond, band1, band2] = filterCorrelationHist(image, feature);
-    band1abs = abs(band1);
-    band2abs = abs(band2);
-    data = [band1abs, band2abs];
-    datasigned = [band1, band2];
+    for i=1:nImages
+        image = load('./VanHateren/im'+string(i)+'.mat').oim;
+        [c,b,hist_joint, hist_cond, band1, band2] = filterCorrelationHist(image, feature);
+        band1abs = abs(band1);
+        band2abs = abs(band2);
+        data = [band1abs, band2abs];
+        datasigned = [band1, band2];
 
-    
-    % Bivariate Maximum Likelihood Estimation of multivariate-t model
-    lastwarn(''); % Clear last warning message
-    [phat_mvt,pci_mvt] = mle(datasigned(:), 'nloglf', @(params,data,cens,freq) -logLikelihood_mvtdist(data,[1 params(1);params(1) 1],params(2), 2), 'Start', [0.5,1], 'Options',statset( 'MaxIter',1e6)); 
-    [warnMsg, warnId] = lastwarn;
-    if isempty(warnMsg) % only save if there was no warning regarding non-convergence of MLE
-        parameterEstimates_mvt(i,1) = phat_mvt(1); % correlation
-        parameterEstimates_mvt(i,2) = phat_mvt(2); % df
-        parameterEstimates_mvt(i,3) = -logLikelihood_mvtdist(datasigned(:),[1 phat_mvt(1);phat_mvt(1) 1],phat_mvt(2), 2); % negative log-likelihood
-        parameterEstimates_mvt(i,4) = 2*2 + parameterEstimates_mvt(i,3); % AIC = 2*nParams - 2*logLikelihood
-    else % MLE did not converge
-        parameterEstimates_mvt(i,:) = nan;
-        %continue % skips MLE of Pareto where mvt did not converge
-    end
-    
 
-    % Bivariate Maximum Likelihood Estimation of Pareto model (with mu=0)
-    lastwarn(''); % Clear last warning message    
-    if homogeneousSigma
-        [phat,pci] = mle(data(:), 'nloglf', @(params,data,cens,freq) -logLikelihood_Pareto(data,[0,0],[params(1),params(1)],params(2),2), 'Start', [1,1], 'Options',statset( 'MaxIter',1e6)); 
-    else
-        [phat,pci] = mle(data(:), 'nloglf', @(params,data,cens,freq) -logLikelihood_Pareto(data,[0,0],[params(1),params(2)],params(3),2), 'Start', [1,1,1], 'Options',statset( 'MaxIter',1e6)); 
-    end
-    [warnMsg, warnId] = lastwarn;
-    if isempty(warnMsg) % only save if there was no warning regarding non-convergence of MLE
-        parameterEstimates_Pareto(i,1) = 0; %mu1
-        parameterEstimates_Pareto(i,2) = 0; %mu2
-        if homogeneousSigma
-            parameterEstimates_Pareto(i,3) = phat(1); %sigma1
-            parameterEstimates_Pareto(i,4) = phat(1); %sigma2
-            parameterEstimates_Pareto(i,5) = phat(2); %beta
-        else
-            parameterEstimates_Pareto(i,3) = phat(1); %sigma1
-            parameterEstimates_Pareto(i,4) = phat(2); %sigma2
-            parameterEstimates_Pareto(i,5) = phat(3); %beta
+        % Bivariate Maximum Likelihood Estimation of multivariate-t model
+        lastwarn(''); % Clear last warning message
+        [phat_mvt,pci_mvt] = mle(datasigned(:), 'nloglf', @(params,data,cens,freq) -logLikelihood_mvtdist(data,[1 params(1);params(1) 1],params(2), 2), 'Start', [0.5,1], 'Options',statset( 'MaxIter',1e6)); 
+        [warnMsg, warnId] = lastwarn;
+        if isempty(warnMsg) % only save if there was no warning regarding non-convergence of MLE
+            parameterEstimates_mvt(i,1) = phat_mvt(1); % correlation
+            parameterEstimates_mvt(i,2) = phat_mvt(2); % df
+            parameterEstimates_mvt(i,3) = -logLikelihood_mvtdist(datasigned(:),[1 phat_mvt(1);phat_mvt(1) 1],phat_mvt(2), 2); % negative log-likelihood
+            parameterEstimates_mvt(i,4) = 2*2 + parameterEstimates_mvt(i,3); % AIC = 2*nParams - 2*logLikelihood
+        else % MLE did not converge
+            parameterEstimates_mvt(i,:) = nan;
+            %continue % skips MLE of Pareto where mvt did not converge
         end
-        parameterEstimates_Pareto(i,6) = -logLikelihood_Pareto(data(:), [0,0], [parameterEstimates_Pareto(i,3),parameterEstimates_Pareto(i,4)], parameterEstimates_Pareto(i,5), 2); % negative log-likelihood
-        parameterEstimates_Pareto(i,7) = 2*3 + parameterEstimates_Pareto(i,6); % AIC = 2*nParams - 2*logLikelihood
-    else
-        parameterEstimates_Pareto(i,:) = nan;
+
+
+        % Bivariate Maximum Likelihood Estimation of Pareto model (with mu=0)
+        lastwarn(''); % Clear last warning message    
+        if homogeneousSigma
+            [phat,pci] = mle(data(:), 'nloglf', @(params,data,cens,freq) -logLikelihood_Pareto(data,[0,0],[params(1),params(1)],params(2),2), 'Start', [1,1], 'Options',statset( 'MaxIter',1e6)); 
+        else
+            [phat,pci] = mle(data(:), 'nloglf', @(params,data,cens,freq) -logLikelihood_Pareto(data,[0,0],[params(1),params(2)],params(3),2), 'Start', [1,1,1], 'Options',statset( 'MaxIter',1e6)); 
+        end
+        [warnMsg, warnId] = lastwarn;
+        if isempty(warnMsg) % only save if there was no warning regarding non-convergence of MLE
+            parameterEstimates_Pareto(i,1) = 0; %mu1
+            parameterEstimates_Pareto(i,2) = 0; %mu2
+            if homogeneousSigma
+                parameterEstimates_Pareto(i,3) = phat(1); %sigma1
+                parameterEstimates_Pareto(i,4) = phat(1); %sigma2
+                parameterEstimates_Pareto(i,5) = phat(2); %beta
+            else
+                parameterEstimates_Pareto(i,3) = phat(1); %sigma1
+                parameterEstimates_Pareto(i,4) = phat(2); %sigma2
+                parameterEstimates_Pareto(i,5) = phat(3); %beta
+            end
+            parameterEstimates_Pareto(i,6) = -logLikelihood_Pareto(data(:), [0,0], [parameterEstimates_Pareto(i,3),parameterEstimates_Pareto(i,4)], parameterEstimates_Pareto(i,5), 2); % negative log-likelihood
+            parameterEstimates_Pareto(i,7) = 2*3 + parameterEstimates_Pareto(i,6); % AIC = 2*nParams - 2*logLikelihood
+        else
+            parameterEstimates_Pareto(i,:) = nan;
+        end
     end
+
+    csvwrite('parameterEstimates_Pareto.csv',[mean(parameterEstimates_Pareto,1,'omitnan'); parameterEstimates_Pareto]); % first row contains mean values
+    csvwrite('parameterEstimates_mvt.csv',[mean(parameterEstimates_mvt,1,'omitnan'); parameterEstimates_mvt]); % first row contains mean values
 end
 
-csvwrite('parameterEstimates_Pareto.csv',[mean(parameterEstimates_Pareto,1,'omitnan'); parameterEstimates_Pareto]); % first row contains mean values
-csvwrite('parameterEstimates_mvt.csv',[mean(parameterEstimates_mvt,1,'omitnan'); parameterEstimates_mvt]); % first row contains mean values
+%% Plotting Goodness-of-Fit and Parameter Estimates
 
 figure;
 set(gcf, 'Units', 'centimeters', 'Position', [0, 0, 5, 5], 'PaperUnits', 'centimeters', 'PaperSize', [5, 5], 'color','w');
@@ -91,10 +99,6 @@ nhist({parameterEstimates_Pareto(:,6),parameterEstimates_mvt(:,3)},'legend',{'Pa
 
 figure;
 nhist({parameterEstimates_Pareto(:,7),parameterEstimates_mvt(:,4)},'legend',{'Pareto','multivariate-t'},'separate','stderror','xlabel','AIC','ylabel','num. of images','fsize',20,'eps','../figures/parameterHisto_AIC_onlymutual.eps'); % nhist: https://www.mathworks.com/matlabcentral/fileexchange/27388-plot-and-compare-histograms-pretty-by-default
-
-
-
-
 
 
 %% Visualization of Histogram for an example image 
